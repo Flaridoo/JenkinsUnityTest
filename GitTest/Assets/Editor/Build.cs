acaf0e2ea8708351f051a16e6f057a7a9947dd1d
Flaridoo <feiyeyu@163.com>
2019-05-31 13:36:38 +0800

    Add Editor folder

---
+++ GitTest/Assets/Editor/Build.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Build : Editor
+{
+
+    [MenuItem ("Tool/Build")]
+    public static void ToBuild()
+    {
+        List<string> levels = new List<string>();
+        foreach(
+        EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if(scene.enabled)
+            {
+                levels.Add(scene.path);
+            }
+        }
+
+        string apkName = "./Test.apk";
+        BuildPipeline.BuildPlayer(levels.ToArray(), apkName, BuildTarget.Android, BuildOptions.None);
+        AssetDatabase.Refresh();
+
+        Debug.Log("Build Done");
+    }
+}
